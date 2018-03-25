c6e896a9dfc2d87b0e37afcd539d78af347f9171
Jordi Mas <jmas@softcatala.org>
2018-03-25 13:44:12 +0200

    Implement service

--- stt-service.py
+++ stt-service.py
@@ -18,13 +18,51 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 
-from flask import Flask
+import os
+import speech_recognition as sr
+from flask import Flask, request
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
+project_path = '/home/jordi/sc/cmusphinx-models-jordi/'
 
-@app.route('/recognize/', methods=['GET'])
+def get_model_path(language=None):
+    language_path = os.path.join(project_path,'ca-ca')
+    hmm_path = os.path.join(language_path,'acoustic-model')
+    lm_file = os.path.join(language_path,'language-model.lm.bin')
+    dict_file = os.path.join(language_path,'pronounciation-dictionary.dict')
+    for prequisit in [language_path, hmm_path, lm_file, dict_file]:
+        if not os.path.exists(prequisit):
+            raise IOError('%s does not exits'%prequisit)
+    return hmm_path, lm_file, dict_file
+
+def recognize(audio_file):
+    language='ca-ca'
+    hmmd, lmd, dictd = get_model_path(language)
+    language_tuple = (hmmd, lmd, dictd)
+
+    r = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        audio = r.record(source)
+
+    return r.recognize_sphinx(audio,language=language_tuple)
+
+#  curl -X POST -F 'file=@test_ca-ca.wav' http://127.0.0.1:5000/recognize/
+@app.route('/recognize/', methods=['POST'])
 def recognize_api():
-    return ("Hola món")
+
+    if 'file' not in request.files:
+        return 'No file part'
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return 'No filename'
+
+    filename = secure_filename(file.filename)
+    file.save(filename)
+    text = recognize(filename)
+    return "text:" + text
 
 if __name__ == '__main__':
     app.debug = True
